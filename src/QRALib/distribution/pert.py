4bb60d8532662f3b4a328bd744b4210c67b35934
samlothen <70292711+samlothen@users.noreply.github.com>
2023-02-28 12:25:51 +0100

    added documentation

--- src/QRALib/distribution/pert.py
+++ src/QRALib/distribution/pert.py
@@ -1,39 +1,79 @@
-"""A impact model based on the Beta-PERT distribution to produce impact modelling
-"""
 from scipy.stats import beta as beta_dist
 
 class PERT:
-    def __init__(self, minimum: float, mid: float, maximum: float):
-        """:param  minimum = Low bound estimate
-        :param mid = Most likely estimate
-        :param maximum = Upper bound estimate
-        The range minimum -> maximum should represent the 90% confidence interval
-        that the loss will fall in that range.
-        """
+    """
+    A model based on the Beta-PERT distribution for impact modelling.
+
+    The Beta-PERT distribution is a three-point estimation technique commonly used in project management
+    to model uncertain variables with a known minimum, most likely, and maximum value.
+
+    :param minimum: The minimum value in the range.
+    :type minimum: float
+    :param mid: The most likely value in the range.
+    :type mid: float
+    :param maximum: The maximum value in the range.
+    :type maximum: float
+    """
+
+    def __init__(self, minimum: float, mid: float, maximum: float) -> None:
         if minimum >= maximum:
             raise AssertionError("Min frequency must exceed max frequency. Min:", minimum, "Max:", maximum)
         if not minimum <= mid <= maximum:
             raise AssertionError("Mean should be between min and max frequencies. Min:", minimum, "Mean:",mid, "Max:", maximum)
+
         self.min = minimum
         self.mid = mid
         self.max = maximum
         self._parameters()
 
-    def _parameters(self):
-        """:param frequency = Mean rate per interval"""
-        self.alpha = (1 + (4 * (self.mid - self.minimum) / (self.maximum - self.minimum) ) )
-        self.beta = (1 + (4 * (self.maximum - self.mid) / (self.maximum - self.minimum) ) )
-        self.location = self.minimum
-        self.scale = self.maximum - self.minimum
-        
-        self.distribution = beta_dist(self.alpha, self.beta, loc=self.location, scale=self.scale)
-        
+    def _parameters(self) -> None:
+        """
+        Calculate the parameters of the Beta-PERT distribution.
+        """
 
-    def draw(self, n=1):
+        self.alpha = (1 + (4 * (self.mid - self.min) / (self.max - self.min) ) )
+        self.beta = (1 + (4 * (self.max - self.mid) / (self.max - self.min) ) )
+        self.location = self.min
+        self.scale = self.max - self.min
+
+        self.distribution = beta_dist(self.alpha, self.beta, loc=self.location, scale=self.scale)
+
+    def sample(self, n: int = 1) -> np.ndarray:
+        """
+        Generate random samples from the Beta-PERT distribution.
+
+        :param n: The number of samples to generate.
+        :type n: int
+        :return: Array of size n with random values from the distribution.
+        :rtype: numpy.ndarray
+        """
+        if not isinstance(n, int) or n <= 0:
+            raise ValueError("n must be a positive integer")
         return self.distribution.rvs(size=n)
 
-    def draw_ppf(self, low_discrepancy_sequences):
-        return self.distribution.ppf(low_discrepancy_sequences)
-    
-    def mean(self):
-        return self.distribution.mean()
+    def sample_ppf(self, percentile_sequences: np.ndarray) -> np.ndarray:
+        """
+        Generate samples from the Beta-PERT distribution using the percent point function (PPF).
+
+        :param percentile_sequences: Array of numbers in the range [0, 1)
+        :type percentile_sequences: numpy.ndarray
+        :return: Array of the same size as the input with values from the distribution PPF
+        :rtype: numpy.ndarray
+        """
+        if not isinstance(percentile_sequences, np.ndarray) or not np.issubdtype(percentile_sequences.dtype, np.number) or not (0 <= percentile_sequences).all() or not (percentile_sequences < 1).all():
+            raise ValueError("percentile_sequences must be a numpy array of numbers in the range [0, 1)")
+        return self.distribution.ppf(percentile_sequences)
+
+    def mean(self, round_to: Optional[int] = None) -> np.float64:
+        """
+        Calculate the mean value of the Beta-PERT distribution.
+
+        :param round_to: Number of decimal places to round the mean value to.
+        :type round_to: int or None
+        :return: Mean value of the distribution.
+        :rtype: numpy.float64
+        """
+        mean = self.distribution.mean()
+        if round_to is not None:
+            mean = round(mean, round_to)
+        return mean
