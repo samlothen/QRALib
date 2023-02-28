b5f47a0ffdd72ac300e9959416a905363ade49d7
samlothen <70292711+samlothen@users.noreply.github.com>
2023-02-28 18:08:12 +0100

    Documenting code
    Updated the documentation

--- src/QRALib/riskportfolio.py
+++ src/QRALib/riskportfolio.py
@@ -4,34 +4,77 @@ from .distributions import uniform as Uniform
 from .distributions import beta as Beta
 from .risk import Risk as Risk
 
-"""
-Note:
-_model object is the object that gets created when calling the various distributions  
-
-"""
 
 class RiskPortfolio:
     """Risk Model"""
 
-    def __init__(self, risk_dict):
+    def __init__(self, risk_dict) -> None:
+        """
+        Initialize a RiskPortfolio object from a dictionary of risks
+        
+        Parameters:
+        ----------
+        risk_dict : dict
+            A dictionary containing a list of risks, where each risk is represented as a dictionary with keys 'ID', 'name', 
+            'frequency', and 'impact'.
+        """
         self.risk_ids = []
         self.instances = []
 
         for risk in risk_dict["Risks"]:
-            uniq_id, name, frequency_dist, frequency_model, impact_dist, impact_model = self._from_dict(risk)
-            
-            self.instances.append(Risk(uniq_id, name, frequency_dist, frequency_model, impact_dist, impact_model))
+            #uniq_id, name, frequency_dist, frequency_model, impact_dist, impact_model = self._from_dict(risk)
+            #self.instances.append(Risk(uniq_id, name, frequency_dist, frequency_model, impact_dist, impact_model))
+            self.instances.append(Risk(*self._from_dict(risk)))
     
     def listing(self):
+        """
+        Return a list of all Risk objects in the portfolio.
+        
+        Returns:
+        -------
+        list
+            A list of all Risk objects in the portfolio.
+        """
         return self.instances
     
     def risk_id_list(self):
+        """
+        Return a list of all risk IDs in the portfolio.
+        
+        Returns:
+        -------
+        list
+            A list of all risk IDs in the portfolio.
+        """
         return self.risk_ids
 
     def search(self, term):
+        """
+        NOT IMPLMENTED YET
+        Search for a risk in the portfolio based on a search term.
+        
+        Parameters:
+        ----------
+        term : str
+            A search term to match against the risk ID or name.
+        """
         pass
 
     def lookup(self, risk_no):
+        """
+        Get the details of a specific risk in the portfolio by its index.
+        
+        Parameters:
+        ----------
+        risk_no : int
+            The index of the risk to lookup.
+            
+        Returns:
+        -------
+        dict
+            A dictionary containing details of the risk, including its ID, name, frequency distribution, frequency 
+            parameters, impact distribution, and impact parameters.
+        """
         risk = {
             "id" : self.instances[risk_no].uniq_id,
             "name" : self.instances[risk_no].name,
@@ -44,8 +87,31 @@ class RiskPortfolio:
         del risk["impact_parameters"]["distribution"]
         return risk
 
-    def _from_dict(self, risk_dict):
-        """Take a Dicitionary object and create a risk"""
+    def _from_dict(self, risk_dict)-> object:
+        """Create a probability distribution object based on the given distribution name.
+
+        Parameters:
+        -----------
+        risk_dict : dict
+            A dictionary containing the parameters of the risk event, including its
+            frequency and impact distribution parameters.
+        distribution : str
+            A string specifying the distribution name to be used (e.g., "Beta", "Lognormal").
+        attribute : str
+            A string specifying which attribute of the risk (i.e., "frequency" or "impact")
+            to apply the distribution to.
+
+        Returns:
+        --------
+        object
+            An object representing the probability distribution of the risk's frequency
+            or impact (e.g., a Beta object, Lognormal object).
+
+        Raises:
+        -------
+        AssertionError:
+            If the input distribution name is not recognized.
+        """
         uniq_id = risk_dict['ID']
         self.risk_ids.append(uniq_id)
         name = risk_dict['name']
@@ -58,7 +124,31 @@ class RiskPortfolio:
         return uniq_id, name, frequency_dist, frequency_model, impact_dist, impact_model
     
     @staticmethod 
-    def _setup_dist(risk_dict, distribution: str, attribute: str):
+    def _setup_dist(risk_dict, distribution: str, attribute: str) -> object:
+        """Create a probability distribution object based on the given distribution name.
+
+        Parameters:
+        -----------
+        risk_dict : dict
+            A dictionary containing the parameters of the risk event, including its
+            frequency and impact distribution parameters.
+        distribution : str
+            A string specifying the distribution name to be used (e.g., "Beta", "Lognormal").
+        attribute : str
+            A string specifying which attribute of the risk (i.e., "frequency" or "impact")
+            to apply the distribution to.
+
+        Returns:
+        --------
+        object
+            An object representing the probability distribution of the risk's frequency
+            or impact (e.g., a Beta object, Lognormal object).
+
+        Raises:
+        -------
+        AssertionError:
+            If the input distribution name is not recognized.
+        """
         if distribution == "Beta":
             model = Beta(risk_dict[attribute]['parameters']['alpha'], risk_dict['frequency']['parameters']['beta'])
         elif distribution == "Uniform":
@@ -70,7 +160,6 @@ class RiskPortfolio:
             mean = risk_dict[attribute]['parameters']['mean']
             maximum = risk_dict[attribute]['parameters']['high']
             model = PERT(minimum, mean, maximum)
-        
         else:
             raise AssertionError("Unknown Distribution, val:", attribute)
         
