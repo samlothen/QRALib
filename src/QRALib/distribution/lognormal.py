948987553e2113992fd83b9c21e868199916ac52
samlothen <70292711+samlothen@users.noreply.github.com>
2023-02-28 12:15:54 +0100

    Added input error handling for each method

--- src/QRALib/distribution/lognormal.py
+++ src/QRALib/distribution/lognormal.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.stats import norm, lognorm
 
 class Lognormal:
-    def __init__(self, low_bound: float, up_bound: float):
+    def __init__(self, low_bound: float, up_bound: float) -> None:
         """
         :param low_bound: Lower bound estimate
         :type low_bound: float
@@ -26,9 +26,9 @@ class Lognormal:
             raise ValueError("up_bound must exceed low_bound")
         self.low_bound = low_bound
         self.up_bound = up_bound
-        self._parameters(low_bound, up_bound)
+        self._parameters()
         
-    def _parameters(self, low_bound, up_bound):
+    def _parameters(self) -> None:
         """
         Calculate the parameters of the Lognormal distribution.
 
@@ -37,11 +37,10 @@ class Lognormal:
         :param up_bound: Upper bound estimate
         :type up_bound: float
         """
-        if low_bound >= up_bound:
-            raise AssertionError("Upper bound must exceed lower bound")
+
         factor = -0.5 / norm.ppf(0.05)
-        self.mu = (math.log(low_bound) + math.log(up_bound)) / 2.  
-        self.sigma = factor * (math.log(up_bound) - math.log(low_bound))   
+        self.mu = (math.log(self.low_bound) + math.log(self.up_bound)) / 2.  
+        self.sigma = factor * (math.log(self.up_bound) - math.log(self.low_bound))   
         self.distribution = lognorm(self.sigma, scale=math.exp(self.mu))
 
 
@@ -54,24 +53,33 @@ class Lognormal:
         :return: Array of size n with random values from distribution
         :rtype: numpy.ndarray
         """
-        return self.distribution.rvs(size=n)
+        if not isinstance(n, int) or n <= 0:
+            raise ValueError("n must be a positive integer")
+        return lognorm.rvs(self.sigma, scale=np.exp(self.mu), size=n)
 
-    def draw_ppf(self, percentile_sequences) -> np.ndarray:
+    def draw_ppf(self, percentile_sequences: np.ndarray) -> np.ndarray:
         """
         Generate samples from the Lognormal distribution using the percent point function (PPF).
 
-        :param percentile_sequences: List of numbers in the range [0, 1]
-        :type percentile_sequences: list
+        :param percentile_sequences: Array of numbers in the range [0, 1)
+        :type percentile_sequences: numpy.ndarray
         :return: Array of the same size as the input with values from the distribution PPF
         :rtype: numpy.ndarray
         """
+        if not isinstance(percentile_sequences, np.ndarray) or not np.issubdtype(percentile_sequences.dtype, np.number) or not (0 <= percentile_sequences).all() or not (percentile_sequences < 1).all():
+            raise ValueError("percentile_sequences must be a numpy array of numbers in the range [0, 1)")
         return self.distribution.ppf(percentile_sequences)
 
-    def mean(self) -> np.float64:
+    def mean(self, round_to: Optional[int] = None) -> np.float64:
         """
         Calculate the mean value of the Lognormal distribution.
-
+    
+        :param round_to: Number of decimal places to round the mean value to
+        :type round_to: int or None
         :return: Mean value of the distribution
         :rtype: numpy.float64
         """
-        return self.distribution.mean()
+        mean = self.distribution.mean()
+        if round_to is not None:
+            mean = round(mean, round_to)
+        return mean
